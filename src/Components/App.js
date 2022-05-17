47316a03bc6c7b56d1b9bce85c456843f9e121b2
Slabstick <slabsticktwitch@gmail.com>
2022-05-17 18:48:15 +0200

    Create new project, push old to comp P1 folder

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,23 +1,15 @@
-import './App.css';
-import React from 'react';
-import { Header } from './Header';
-import { H1 } from './H1';
-import { List } from './List';
-import { Footer } from './Footer';
+import React from "react";
+import Main from "./Main";
+import Navbar from "./Navbar";
+import './App.css'
 
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <H1 />
-        <List />
-        <Footer />
-      </div>
-    );
-  }
-  
+export default class App extends React.Component {
+    render() {
+        return(
+            <div>
+                <Navbar />
+                <Main />
+            </div>
+        )
+    }
 }
-
-export default App;
