363d123ff8c67b32c79dd0464b2172d99876d14f
Slabstick <slabsticktwitch@gmail.com>
2022-05-19 10:18:31 +0200

    add new project for jsx introduction

---
+++ src/JSXIntroduction/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Contacts from "./Contacts"
+
+
+export default class App extends React.Component {
+    render () {
+        return(
+            <div>
+                <Contacts />
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Airbnb/App';
+import App from './JSXIntroduction/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
