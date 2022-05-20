a86543862237fd3473adb6dcd890e0a342aea54d
Slabstick <slabsticktwitch@gmail.com>
2022-05-20 19:48:23 +0200

    add functionality to get and display images

--- src/Meme Generator/Meme.js
+++ src/Meme Generator/Meme.js
@@ -6,27 +6,30 @@ import memesData from './memesData';
 
 function Meme() {
 
-	function handleClick() {
-	const rand = Math.floor(Math.random() * memesData.data.memes.length)
-	console.log(memesData.data.memes[rand].url);
+	const [memeImage, setMemeImage] = React.useState("");
+
+	function getMemeImage() {
+	const rand = Math.floor(Math.random() * memesData.data.memes.length);
+	setMemeImage(memesData.data.memes[rand].url);
 	}
-		return (
-			<div className='meme-container'>
-				<div className='form'>
 
-						<input type={'text'} id={'textup'} name={'textup'} placeholder={'Top text'} className='text'></input>
-						<input type={'text'} id={'textdown'} name={'textdown'} placeholder={'Bottom text'} className='text'></input>
-
-					<input 
-					type={'submit'} 
-					value={'Get a new meme image 🖼'} 
-					className='button'>
-					onClick={handleClick}
-					</input>
-				</div>
+	return (
+		<div className='meme-container'>
+			<div className='form'>
+					<input type={'text'} id={'textup'} name={'textup'} placeholder={'Top text'} className='text'></input>
+					<input type={'text'} id={'textdown'} name={'textdown'} placeholder={'Bottom text'} className='text'></input>
+				<input 
+				type={'submit'} 
+				value={'Get a new meme image 🖼'} 
+				className='button'
+				onClick={getMemeImage}
+				>
+				</input>
 			</div>
-		)
-	}
+			<img src={memeImage} alt="" className='meme-image'/>
+		</div>
+	)
+}
 
 
 export default Meme;
